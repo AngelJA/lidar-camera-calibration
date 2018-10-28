6f2fef576214182d4cd18036874c67ca76096a31
Angel Avila <angel.j.avila@gmail.com>
2018-10-28 16:56:08 -0700

    fixed docker mount path to be relative to pwd

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 docker build -t calib-test .
 xhost +
-nvidia-docker run -ti --rm -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY -v ~/calib-test/ws:/root/ws --name calib-test-container --privileged --net host calib-test
+nvidia-docker run -ti --rm -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY -v `pwd`/ws:/root/ws --name calib-test-container --privileged --net host calib-test
 xhost -
