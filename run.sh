96c0c463275d096f7b67427995cd6dfee3ff606d
Angel Avila <angel.j.avila@gmail.com>
2018-10-28 16:42:11 -0700

    moved build command into run scripts

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
+docker build -t calib-test .
 xhost +
 nvidia-docker run -ti --rm -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY -v ~/calib-test/ws:/root/ws --name calib-test-container --privileged --net host calib-test
 xhost -
