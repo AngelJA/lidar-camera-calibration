73746cc01b1add136b3ef2211ebd1aa6990273b6
Angel Avila <angel.j.avila@gmail.com>
2019-03-18 02:31:08 -0700

    ported most of the python code to cpp chessboard detection done, need to do svd to finish solving for camera values image with detected corners being published

--- ws/src/lidar_camera_calibration/include/lidar_camera_calibration.h
+++ ws/src/lidar_camera_calibration/include/lidar_camera_calibration.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <ros/ros.h>
-#include <Eigen/Dense>
-#include <unordered_map>
+
+// opencv related
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/calib3d.hpp>
 
 // pcl related
 #include <pcl_conversions/pcl_conversions.h>
@@ -15,9 +17,11 @@
 
 // messages
 #include <geometry_msgs/PointStamped.h>
+using geometry_msgs::Point;
 using geometry_msgs::PointStampedConstPtr;
 
 #include <sensor_msgs/Image.h>
+using sensor_msgs::Image;
 using sensor_msgs::ImageConstPtr;
 
 #include <sensor_msgs/PointCloud2.h>
@@ -29,6 +33,7 @@ using visualization_msgs::Marker;
 
 class LidarCameraCalibration
 {
+public:
     LidarCameraCalibration();
 
     // callbacks
@@ -36,12 +41,32 @@ class LidarCameraCalibration
     void ImageCallback(ImageConstPtr msg);
     void PointCloudCallback(PointCloud2ConstPtr msg);
 
+private:
+    // lidar processing
+    pcl::PointCloud<pcl::PointXYZ>::Ptr FilterPointCloud(Point point);
+    void PerformPlaneFit(pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud);
+    void PublishPlaneMarker(Point point);
+    pcl::ModelCoefficients::Ptr plane_model_coefficients;
+
+    // camera processing
+    int checkerboard_rows;
+    int checkerboard_columns;
+    double checkerboard_square_size;
+    cv::Mat_<double> checkerboard_points;
+
     // data
-    PointCloud2ConstPtr point_cloud;
+    PointCloud2ConstPtr last_point_cloud;
     ImageConstPtr image;
 
+    std::vector<cv::Vec<double, 3>> theta_lidar;
+    std::vector<double> alpha_lidar;
+    std::vector<cv::Vec<double, 3>> theta_camera;
+    std::vector<double> alpha_camera;
 
+    // ros stuff
     ros::NodeHandle nh;
     std::vector<ros::Subscriber> subscribers;
-    std::unordered_map<std::string, ros::Publisher> publishers;
+    ros::Publisher marker_publisher;
+    ros::Publisher processed_image_publisher;
+    ros::Publisher pc_test_pub;
 };
--- ws/src/lidar_camera_calibration/src/lidar_camera_calibration.cpp
+++ ws/src/lidar_camera_calibration/src/lidar_camera_calibration.cpp
@@ -1,83 +1,178 @@
 #include <lidar_camera_calibration.h>
 
+#include <iostream>
 
-#include <std_msgs/Float32MultiArray.h>
+LidarCameraCalibration::LidarCameraCalibration() : nh("~")
+{
+    this->subscribers.push_back(this->nh.subscribe("/clicked_point", 1, &LidarCameraCalibration::ClickedPointCallback, this));
+    this->subscribers.push_back(this->nh.subscribe("image", 1, &LidarCameraCalibration::ImageCallback, this));
+    this->subscribers.push_back(this->nh.subscribe("lidar_points", 1, &LidarCameraCalibration::PointCloudCallback, this));
+    
+    this->marker_publisher = this->nh.advertise<Marker>("markers", 1);
+    this->processed_image_publisher = this->nh.advertise<Image>("processed_image", 1);
+    this->pc_test_pub = this->nh.advertise<PointCloud2>("test_pc", 1);
 
-using std_msgs::Float32MultiArray;
-using std_msgs::MultiArrayDimension;
-using geometry_msgs::Point;
+    assert(this->nh.getParam("checkerboard/rows", this->checkerboard_rows));
+    assert(this->nh.getParam("checkerboard/columns", this->checkerboard_columns));
+    assert(this->nh.getParam("checkerboard/square_size", this->checkerboard_square_size));
+    this->checkerboard_points = cv::Mat_<double>(this->checkerboard_rows * this->checkerboard_columns, 3, CV_64F);
+    for (int i = 0; i < this->checkerboard_rows * this->checkerboard_columns; ++i)
+    {
+        this->checkerboard_points(i, 0) = (i / this->checkerboard_rows) * this->checkerboard_square_size;
+        this->checkerboard_points(i, 1) = (i % this->checkerboard_rows) * this->checkerboard_square_size;
+        this->checkerboard_points(i, 2) = 0.0;
+    }
 
-ros::Publisher segment_pub, point_cloud_pub, inliers_pub, model_plane_pub, arrow_pub;
+    // segment_pub = nh.advertise<PointCloud2>("segment_point_cloud", 1);
+    // point_cloud_pub = nh.advertise<PointCloud2>("plane_point_cloud", 1);
+    // model_plane_pub = nh.advertise<Float32MultiArray>("model_plane_coeffs", 1);
+    ros::spin();
+}
 
-// void ClickedPointCallback(PointStampedConstPtr msg)
-// {
-//     pcl::PointCloud<pcl::PointXYZI>::Ptr cloud = boost::make_shared<pcl::PointCloud<pcl::PointXYZI>>();
-//     pcl::fromROSMsg(*point_cloud, *cloud);
+void LidarCameraCalibration::ClickedPointCallback(PointStampedConstPtr msg)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_point_cloud = this->FilterPointCloud(msg->point);
+    
+    PointCloud2 test;
+    pcl::toROSMsg(*filtered_point_cloud, test);
+    this->pc_test_pub.publish(test);
 
-//     // segment out just the area around the clicked point
-//     double d = 0.25;
-//     pcl::PointIndices::Ptr remove_indices = boost::make_shared<pcl::PointIndices>();
-//     for (size_t i = 0; i < cloud->size(); ++i)
-//     {
-//         if (cloud->points[i].x < msg->point.x - d ||
-//             cloud->points[i].x > msg->point.x + d ||
-//             cloud->points[i].y < msg->point.y - d ||
-//             cloud->points[i].y > msg->point.y + d ||
-//             cloud->points[i].z < msg->point.z - d ||
-//             cloud->points[i].z > msg->point.z + d)
-//             {
-//                 remove_indices->indices.push_back(i);
-//             }
-//     }
+    this->PerformPlaneFit(filtered_point_cloud);
 
-//     pcl::ExtractIndices<pcl::PointXYZI> extract;
-//     extract.setInputCloud(cloud);
-//     extract.setIndices(remove_indices);
-//     extract.setNegative(true);
-//     extract.filter(*cloud);
+}
 
-//     PointCloud2 msg_out;
-//     pcl::toROSMsg(*cloud, msg_out);
-//     segment_pub.publish(msg_out);
+void LidarCameraCalibration::ImageCallback(ImageConstPtr msg)
+{
+    this->image = msg;
 
-//     // find plane fit
-//     pcl::ModelCoefficients::Ptr coeff(new pcl::ModelCoefficients());
-//     pcl::PointIndices::Ptr inliers(new pcl::PointIndices());
-//     pcl::SACSegmentation<pcl::PointXYZI> seg;
-//     seg.setOptimizeCoefficients(true);
-//     seg.setModelType(pcl::SACMODEL_PLANE);
-//     seg.setMethodType(pcl::SAC_RANSAC);
-//     seg.setDistanceThreshold(0.03);
-//     seg.setInputCloud(cloud);
-//     seg.segment(*inliers, *coeff);
+    cv_bridge::CvImageConstPtr image_ptr = cv_bridge::toCvShare(msg);
+    cv_bridge::CvImageConstPtr gray_image_ptr = cv_bridge::toCvShare(msg, "mono8");
+    std::vector<cv::Point2f> corners;
+    cv::Size pattern_size(this->checkerboard_rows, this->checkerboard_columns);
+    bool pattern_found = cv::findChessboardCorners(
+        gray_image_ptr->image,
+        pattern_size,
+        corners,
+        cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE + cv::CALIB_CB_FAST_CHECK);
 
-//     // make sure plane normal points in negative x
-//     if (coeff->values[0] > 0) {
-//         coeff->values[0] *= -1;
-//         coeff->values[1] *= -1;
-//         coeff->values[2] *= -1;
-//     }
+    if (pattern_found)
+    {
+        // refine detected corners
+        // Use a radius of half the minimum distance between corners. This should be large enough to snap to the
+        // correct corner, but not so large as to include a wrong corner in the search window.
+        auto dist = [](cv::Point2f& a, cv::Point2f& b){ return cv::sqrt(std::pow(a.x - b.x, 2) + std::pow(a.y - b.y, 2)); };
+        double min_distance = dist(corners[0], corners[1]);
+        for (int i = 1; i < corners.size(); ++i)
+        {
+            if ((i + 1) % this->checkerboard_columns)
+            {
+                min_distance = std::min(min_distance, dist(corners[i], corners[i + 1]));
+            }
+            if (i + this->checkerboard_columns < corners.size())
+            {
+                min_distance = std::min(min_distance, dist(corners[i], corners[i + this->checkerboard_columns]));
+            }
+        }
 
-//     extract.setNegative(false);
-//     extract.setIndices(inliers);
-//     extract.filter(*cloud);
+        int radius = std::ceil(min_distance * 0.5);
+        cv::cornerSubPix(
+            gray_image_ptr->image,
+            corners,
+            cv::Size(radius, radius),
+            cv::Size(-1, -1),
+            cv::TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
 
-//     PointCloud2 inliers_out;
-//     pcl::toROSMsg(*cloud, inliers_out);
-//     inliers_pub.publish(inliers_out);
+        // // still needs to be ported from python
+        // if corners[0, 0, 1] > corners[-1, 0, 1]:
+        //     numpy.flipud(corners)
 
-//     // publish model coefficients
-//     Float32MultiArray coeff_msg;
-//     coeff_msg.layout.dim.push_back(MultiArrayDimension());
-//     coeff_msg.layout.dim[0].size = 4;
-//     coeff_msg.layout.dim[0].stride = 1;
-//     coeff_msg.layout.dim[0].label = "coefficients";
-//     for (float val : coeff->values)
-//     {
-//         coeff_msg.data.push_back(val);
-//     }
-//     model_plane_pub.publish(coeff_msg);
+        // ret, r, t = cv2.solvePnP(self._object_points, corners, self._camera_matrix, (0, 0, 0, 0))
+        // R, _ = cv2.Rodrigues(r)
 
+        // self._last_theta_c = numpy.array([R[:, 2]]).T
+        // self._last_alpha_c = t.T.dot(self._last_theta_c)
+
+        cv::drawChessboardCorners(image_ptr->image, pattern_size, corners, pattern_found);
+    }
+
+
+    this->processed_image_publisher.publish(image_ptr->toImageMsg());
+}
+
+void LidarCameraCalibration::PointCloudCallback(PointCloud2ConstPtr msg)
+{
+    this->last_point_cloud = msg;
+}
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr LidarCameraCalibration::FilterPointCloud(Point point)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+    pcl::fromROSMsg(*this->last_point_cloud, *cloud);
+
+    double d = 0.25;
+    pcl::PointIndices::Ptr remove_indices(new pcl::PointIndices);
+    for (size_t i = 0; i < cloud->size(); ++i)
+    {
+        if (cloud->points[i].x < point.x - d ||
+            cloud->points[i].x > point.x + d ||
+            cloud->points[i].y < point.y - d ||
+            cloud->points[i].y > point.y + d ||
+            cloud->points[i].z < point.z - d ||
+            cloud->points[i].z > point.z + d)
+            {
+                remove_indices->indices.push_back(i);
+            }
+    }
+
+    pcl::ExtractIndices<pcl::PointXYZ> extract;
+    extract.setInputCloud(cloud);
+    extract.setIndices(remove_indices);
+    extract.setNegative(true);
+    extract.filter(*cloud);
+
+    return cloud;
+}
+
+void LidarCameraCalibration::PerformPlaneFit(pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud)
+{
+    pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+    pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+
+    pcl::SACSegmentation<pcl::PointXYZ> seg;
+    seg.setOptimizeCoefficients(true);
+    seg.setModelType(pcl::SACMODEL_PLANE);
+    seg.setMethodType(pcl::SAC_RANSAC);
+    seg.setDistanceThreshold(0.03);
+    seg.setInputCloud(point_cloud);
+    seg.segment(*inliers, *coefficients);
+
+    // make sure plane normal points in negative x
+    if (coefficients->values[0] > 0) {
+        coefficients->values[0] *= -1;
+        coefficients->values[1] *= -1;
+        coefficients->values[2] *= -1;
+    }
+
+    this->theta_lidar.push_back(cv::Vec<double, 3>(
+        coefficients->values[0],
+        coefficients->values[1],
+        coefficients->values[2]));
+    this->alpha_lidar.push_back(coefficients->values[3]);
+
+    cv::Mat_<double> m(3, this->theta_lidar.size(), CV_64F);
+    for (int i = 0; i < this->theta_lidar.size(); ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            m(j, i) = this->theta_lidar[i][j];
+        }
+    }
+    std::cout << "matrix = " << m << std::endl;
+}
+
+void PublishPlaneMarker(Point point)
+{
 //     // draw arrow at clicked point representing plane normal
 //     //double length = std::sqrt(std::pow(coeff->values[0], 2) + std::pow(coeff->values[1], 2) + std::pow(coeff->values[2], 2));
 //     Marker arrow;
@@ -100,40 +195,11 @@ ros::Publisher segment_pub, point_cloud_pub, inliers_pub, model_plane_pub, arrow
 //     arrow.color.a = 1;
 //     arrow.color.r = 1;
 //     arrow_pub.publish(arrow);
-// }
-
-LidarCameraCalibration::LidarCameraCalibration()
-{
-    this->subscribers.push_back(this->nh.subscribe("/clicked_point", 1, &LidarCameraCalibration::ClickedPointCallback, this));
-    this->subscribers.push_back(this->nh.subscribe("lidar_points", 1, &LidarCameraCalibration::PointCloudCallback, this));
-    
-    this->publishers["inliers"] = this->nh.advertise<PointCloud2>("inliers", 1);
-    this->publishers["markers"] = this->nh.advertise<Marker>("markers", 1);
-
-    // segment_pub = nh.advertise<PointCloud2>("segment_point_cloud", 1);
-    // point_cloud_pub = nh.advertise<PointCloud2>("plane_point_cloud", 1);
-    // model_plane_pub = nh.advertise<Float32MultiArray>("model_plane_coeffs", 1);
-    ros::spin();
-}
-
-void LidarCameraCalibration::ClickedPointCallback(PointStampedConstPtr msg)
-{
-
-}
-
-void LidarCameraCalibration::ImageCallback(ImageConstPtr msg)
-{
-    this->image = msg;
-}
-
-void LidarCameraCalibration::PointCloudCallback(PointCloud2ConstPtr msg)
-{
-    this->point_cloud = msg;
 }
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "lidar_camera_calibration_node");
-    LidarCameraCalibration node();
+    LidarCameraCalibration node;
     return 0;
 }
